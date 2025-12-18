7aff3c881810e33b2b5915964ad49fa20c2a9158
Anish Pallati <i@anish.land>
2025-12-18 07:12:27 -0500

    feat: add nix config

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Discord Verify bot";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      nixosModules.default = import ./nix/module.nix { inherit self; };
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "discord-verify";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl ];
+          };
+        }
+      );
+    };
+}
---
+++ nix/module.nix
@@ -0,0 +1,52 @@
+{ self }:
+
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.services.discord-verify;
+in
+{
+  options.services.discord-verify = {
+    enable = lib.mkEnableOption "Discord Verify bot";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      description = "The discord-verify package to use";
+    };
+
+    environmentFile = lib.mkOption {
+      type = lib.types.path;
+      description = "Path to environment file containing all configuration";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    users.users.discord-verify = {
+      isSystemUser = true;
+      group = "discord-verify";
+    };
+    users.groups.discord-verify = {};
+
+    systemd.services.discord-verify = {
+      description = "Discord Verify Bot";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+        User = "discord-verify";
+        Group = "discord-verify";
+        EnvironmentFile = cfg.environmentFile;
+        ExecStart = "${cfg.package}/bin/discord-verify";
+        Restart = "on-failure";
+        RestartSec = "10s";
+
+        NoNewPrivileges = true;
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        PrivateTmp = true;
+      };
+    };
+  };
+}
